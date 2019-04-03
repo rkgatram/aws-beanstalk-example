edb835544537614bd317d77d93e26d69f544a436
ravikumar.g <rkgatram@gmail.com>
2019-04-03 23:41:38 +0530

    AWS Beanstalk example with CICD

---
+++ src/main/java/com/rkgatram/aws/AwsElasticBeasnstalkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.rkgatram.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeasnstalkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeasnstalkExampleApplication.class, args);
+	}
+
+}
